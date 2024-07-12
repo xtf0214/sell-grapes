219243b69bfc3b4af5fc985ad1f9ef83ab5d23a0
Tanphoon <2979283542@qq.com>
2024-07-12 22:58:46 +0800

    增加历史销售查看功能

--- js/count.js
+++ js/count.js
@@ -1,63 +1,121 @@
-let soldList = [];
-
+let dayList = [];
 function loadData() {
-    const today = new Date().toLocaleDateString();
-    if (localStorage.getItem(today) == null) {
-        localStorage.setItem(today, JSON.stringify({ 'soldList': soldList }));
+    if (localStorage.getItem("dayList") == null) {
+        localStorage.setItem("dayList", JSON.stringify([]));
     } else {
-        const tmp = JSON.parse(localStorage.getItem(today));
-        soldList = tmp.soldList;
+        dayList = JSON.parse(localStorage.getItem("dayList"));
     }
 }
-function timeCount() {
-    const startTime = soldList[soldList.length - 1]['time'].substring(0, 5);
-    const endTime = soldList[0]['time'].substring(0, 5);
+function loadSalesList(day) {
+    if (localStorage.getItem(day) == null) {
+        localStorage.setItem(day, JSON.stringify({ "salesList": [] }));
+        return [];
+    } else {
+        const tmp = JSON.parse(localStorage.getItem(day));
+        return tmp.salesList
+    }
+}
+function calcSalesAmount(day, paymentMethod = "总计") {
+    let salesList = loadSalesList(day);
+    let res = 0;
+    salesList.forEach(item => {
+        if (paymentMethod == "总计" || item["paymentMethod"] == paymentMethod) {
+            res += parseFloat(item["amount"]);
+        }
+    });
+    return res;
+}
+function calcSalesVolume(day) {
+    let salesList = loadSalesList(day);
+    let res = 0;
+    salesList.forEach(item => {
+        res += parseFloat(item["amount"]) / parseFloat(item["price"]);
+    });
+    return res;
+}
+function calcTimeSalesStatistic(day) {
+    const salesList = loadSalesList(day);
+    const startTime = salesList[salesList.length - 1]["time"].substring(0, 5);
+    const endTime = salesList[0]["time"].substring(0, 5);
     const timeRange = generateTimeRange(startTime, endTime);
-    let totalSales = 0, totalQuantity = 0;
-    let totalWechatSales = 0, totalCashSales = 0;
-    let soldCountByTime = {};
+    let timeSalesList = {};
     timeRange.forEach(time => {
-        soldCountByTime[time] = 0;
+        timeSalesList[time] = 0;
     });
-    soldList.forEach(item => {
-        const time = item['time'].substring(0, 5);
-        const amount = parseFloat(item['amount']);
-        const price = parseFloat(item['price']);
-        const payment = item["paymentMethod"];
-        soldCountByTime[time] += amount;
-        totalSales += amount;
-        totalQuantity += amount / price;
-        if (payment == "微信")
-            totalWechatSales += amount;
-        else
-            totalCashSales += amount;
+    salesList.forEach(item => {
+        timeSalesList[item["time"].substring(0, 5)] += parseFloat(item["amount"]);
     });
-    document.querySelector("#totalSales").textContent = '总销售额：' + totalSales + ' 元';
-    document.querySelector("#totalWeChatSales").textContent = '微信总额：' + totalWechatSales + ' 元';
-    document.querySelector("#totalCashSales").textContent = '现金总额：' + totalCashSales + ' 元';
-    document.querySelector("#totalQuantity").textContent = '总销售量：' + parseInt(totalQuantity) + ' 斤';
-    const timeCountctx = document.getElementById('timeCount');
-    new Chart(timeCountctx, {
-        type: 'bar',
+    return timeSalesList;
+}
+function salesStatistic() {
+    const today = new Date().toLocaleDateString();
+    const salesList = loadSalesList(today);
+    let totalSalesAmount = calcSalesAmount(today);
+    let totalSalesVolume = calcSalesVolume(today);
+    let totalWeChatSales = calcSalesAmount(today, "微信");
+    let totalCashSales = calcSalesAmount(today, "现金");
+    document.querySelector("#totalSales").textContent = "今日总收入：" + totalSalesAmount + " 元";
+    document.querySelector("#totalWeChatSales").textContent = "今日微信收入：" + totalWeChatSales + " 元";
+    document.querySelector("#totalCashSales").textContent = "今日现金收入：" + totalCashSales + " 元";
+    document.querySelector("#totalQuantity").textContent = "今日总销售量：" + Math.round(totalSalesVolume) + " 斤";
+}
+function timeStatisticChart() {
+    const today = new Date().toLocaleDateString();
+    const timeSalesList = calcTimeSalesStatistic(today);
+    const ctx = document.getElementById("timeStatisticChart");
+    new Chart(ctx, {
+        type: "bar",
         data: {
-            labels: Object.keys(soldCountByTime),
+            labels: Object.keys(timeSalesList),
             datasets: [{
-                label: '金额',
-                data: Object.values(soldCountByTime),
+                label: "金额",
+                data: Object.values(timeSalesList),
                 borderWidth: 2
             }]
         },
     });
 }
+function dayStatisticChart() {
+    let weChatSalesList = [];
+    let cashSalesList = [];
+    let totalSalesList = [];
+    dayList.forEach(day => {
+        totalSalesList.push(calcSalesAmount(day));
+        cashSalesList.push(calcSalesAmount(day, "现金"));
+        weChatSalesList.push(calcSalesAmount(day, "微信"));
+    });
+    const ctx = document.getElementById("dayStatisticChart");
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: dayList,
+            datasets: [
+                {
+                    label: "总计",
+                    data: totalSalesList,
+                    borderWidth: 2
+                }, {
+                    label: "微信",
+                    data: weChatSalesList,
+                    borderWidth: 2
+                }, {
+                    label: "现金",
+                    data: cashSalesList,
+                    borderWidth: 2
+                }
+            ]
+        },
+    });
+}
+
 // 生成完整的时间范围
 function generateTimeRange(start, end) {
-    var startTime = new Date('2000-01-01 ' + start);
-    var endTime = new Date('2000-01-01 ' + end);
+    var startTime = new Date("2000-01-01 " + start);
+    var endTime = new Date("2000-01-01 " + end);
     var timeRange = [];
-    while (startTime <= endTime) {
+    for (; startTime <= endTime; startTime.setMinutes(startTime.getMinutes() + 1)) {
         var time = startTime.toTimeString().substring(0, 5);
         timeRange.push(time);
-        startTime.setMinutes(startTime.getMinutes() + 1);
     }
     return timeRange;
 }
--- js/main.js
+++ js/main.js
@@ -1,61 +1,67 @@
 const App = {
-	data() {
-		return {
-			amount: '',
-			price: '10',
-			paymentMethod: '',
-			soldList: [],
-		};
-	},
-	created() {
-		const today = new Date().toLocaleDateString();
-		if (localStorage.getItem(today) == null) {
-			localStorage.setItem(today, JSON.stringify({
-				'soldList': this.soldList
-			}));
-		} else {
-			const tmp = JSON.parse(localStorage.getItem(today));
-			this.soldList = tmp.soldList;
-		}
-	},
-	methods: {
-		saveData() {
-			const today = new Date().toLocaleDateString();
-			localStorage.setItem(today, JSON.stringify({
-				'soldList': this.soldList
-			}));
-		},
-		submitForm() {
-			if (this.amount && this.paymentMethod) {
-				const curTime = new Date().toLocaleTimeString();
-				this.soldList.unshift({
-					time: curTime,
-					amount: parseFloat(this.amount),
-					price: parseFloat(this.price),
-					paymentMethod: this.paymentMethod,
-				});
-				localStorage.setItem('paymentAmount', this.amount);
-				localStorage.setItem('paymentMethod', this.paymentMethod);
-				this.saveData();
-			} else {
-				alert('请填写所有字段');
-			}
-		},
-		revoke() {
-			if (this.soldList.length > 0) {
-				this.soldList.shift();
-				this.saveData();
-			}
-		},
-		clearNum() {
-			this.menuItems.forEach(item => {
-				item.rest = 0;
-				item.sold = 0;
-			});
-			this.soldList.splice(0, this.soldList.length);
-			const today = new Date().toLocaleDateString();
-			localStorage.removeItem(today);
-		},
-	}
+    data() {
+        return {
+            amount: '',
+            price: '10',
+            paymentMethod: '',
+            salesList: [],
+        };
+    },
+    created() {
+        const today = new Date().toLocaleDateString();
+        if (localStorage.getItem(today) == null) {
+            localStorage.setItem(today, JSON.stringify({
+                'salesList': this.salesList
+            }));
+            if (localStorage.getItem('dayList') == null) {
+                localStorage.setItem('dayList', JSON.stringify([]));
+            } else {
+                let dayList = JSON.parse(localStorage.getItem('dayList'));
+                dayList.push(today);
+            }
+        } else {
+            const tmp = JSON.parse(localStorage.getItem(today));
+            this.salesList = tmp.salesList;
+        }
+    },
+    methods: {
+        saveData() {
+            const today = new Date().toLocaleDateString();
+            localStorage.setItem(today, JSON.stringify({
+                'salesList': this.salesList
+            }));
+        },
+        submitForm() {
+            if (this.amount && this.paymentMethod) {
+                const curTime = new Date().toLocaleTimeString();
+                this.salesList.unshift({
+                    time: curTime,
+                    amount: parseFloat(this.amount),
+                    price: parseFloat(this.price),
+                    paymentMethod: this.paymentMethod,
+                });
+                localStorage.setItem('paymentAmount', this.amount);
+                localStorage.setItem('paymentMethod', this.paymentMethod);
+                this.saveData();
+            } else {
+                alert('请填写所有字段');
+            }
+        },
+        revoke() {
+            if (this.salesList.length > 0) {
+                this.salesList.shift();
+                this.saveData();
+            }
+        },
+        clearNum() {
+            this.menuItems.forEach(item => {
+                item.rest = 0;
+                item.sold = 0;
+            });
+            this.salesList.splice(0, this.salesList.length);
+            const today = new Date().toLocaleDateString();
+            localStorage.removeItem(today);
+        },
+    }
 };
 Vue.createApp(App).mount('#app');
